b05befc596b84f1f11167494ff42406431e46f52
Hanna Ponomarenko <pnmrnk.anna@gmail.com>
2025-05-02 19:53:51 +0200

    add styles

---
+++ src/js/2-form.js
@@ -0,0 +1,11 @@
+// const formData = {
+//   email: "",
+//   message: ""
+// };
+
+// const LS_KEY = "feedback-form-state";
+
+// const form = document.querySelector(".feedback-form");
+// const inputEmail = document.querySelector("[name='email']"),
+// const textareaMessage = document.querySelector("[name='message']")
+
